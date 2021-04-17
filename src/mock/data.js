07168696c990b42565f4af265437852e504c2c7e
Eamon <eamon0989@gmail.com>
2021-04-17 22:39:58 +0200

    changed subtitle

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Hi, my name is ',
   name: `Eamon O'Callaghan`,
-  subtitle: `I'm a JS and PHP Developer`,
+  subtitle: `I'm a Junior JavaScript and PHP Developer`,
   cta: 'Learn more',
 };
 
