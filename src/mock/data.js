c5e9a2c6284b2bae8dcd985f34aff1b411120300
Eamon <eamon0989@gmail.com>
2021-04-17 02:50:32 +0200

    Added missing title

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,7 +9,7 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
+  title: 'Hi, my name is',
   name: `Eamon O'Callaghan`,
   subtitle: `I'm a JS and PHP Developer`,
   cta: 'Learn more',
